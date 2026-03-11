84cb8094f047546418a92d01e91d6d0a01cd900d
mateoAlonso06 <alonsomateo2002@gmail.com>
2026-03-11 14:47:03 -0300

    fix: arregla errores de estilo/sintaxis PSR12

--- public/index.php
+++ public/index.php
@@ -24,12 +24,15 @@ $router->set404(function () {
 
 $jwtMiddleware = new JwtMiddleware(new JwtTokenProvider());
 
-$router->before('GET|POST|PUT|DELETE|PATCH', '/(?!auth\/login|auth\/register|docs\/).*',
+$router->before(
+    'GET|POST|PUT|DELETE|PATCH',
+    '/(?!auth\/login|auth\/register|docs\/).*',
     function () use ($jwtMiddleware) {
         if (!$jwtMiddleware->handle()) {
             exit();
         }
-    });
+    }
+);
 
 require_once __DIR__ . '/../routes/auth.php';
 require_once __DIR__ . '/../routes/materia.php';
